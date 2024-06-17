6ccaa0f8059b3be0d2902108efdb7bb9244e6490
Banggg <133222169+Bangwoo4@users.noreply.github.com>
2024-06-17 19:45:09 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <Header />
+      </div>
+
+      <div className="Main"></div>
+
+      <div className="Footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2025 Weather Website</p>
+      <nal>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+      </nal>
+    </footer>
+  );
+};
+
+export default Footer;
